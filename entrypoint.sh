208c1b966266653bb3e67e1ed0bd0c17e9140f45
taylorwentzel <66229191+taylorwentzel@users.noreply.github.com>
2020-06-24 15:44:20 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ chmod +x
 set -e
 GIT_LOG=`git log --pretty="%an %ae%n%cn %ce" | sort | uniq`
 function join { local IFS="$1"; shift; echo "$*"; }
-log_data=$(join '\n' ${GIT_LOG[@]})
+log_data=$(join , ${GIT_LOG[@]})
 generate_post_data()
 {
   cat <<EOF
